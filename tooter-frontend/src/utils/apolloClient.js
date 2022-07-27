6b85cbb0d5daf905f754e144eaf05ba23e2640b9
DDQuin <ddquin420@gmail.com>
2022-07-27 16:59:09 +0100

    harcode url apollo client again

--- tooter-frontend/src/utils/apolloClient.js
+++ tooter-frontend/src/utils/apolloClient.js
@@ -20,10 +20,10 @@ const authLink = setContext((_, { headers }) => {
     };
 });
 
-const httpLink = new HttpLink({ uri: `http://localhost:${process.env.REACT_APP_PORT}` });
+const httpLink = new HttpLink({ uri: `http://localhost:4000` });
 
 const wsLink = new WebSocketLink({
-    uri: `ws://localhost:${process.env.REACT_APP_PORT}/graphql`,
+    uri: `ws://localhost:4000/graphql`,
     options: {
         reconnect: true,
     },
