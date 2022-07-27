8d65d818bdb2e5e35c0e5871903ae105b34ea579
DDQuin <ddquin420@gmail.com>
2022-07-27 21:39:01 +0100

    removed web socket

--- tooter-frontend/src/index.js
+++ tooter-frontend/src/index.js
@@ -7,14 +7,11 @@ import {ApolloProvider} from "@apollo/client";
 import {
     ApolloClient,
     HttpLink,
-    split,
     InMemoryCache,
 } from "@apollo/client";
 
 import { setContext } from "@apollo/client/link/context";
 
-import { getMainDefinition } from "@apollo/client/utilities";
-import { WebSocketLink } from "@apollo/client/link/ws";
 import axios from "axios";
 
 axios
@@ -23,41 +20,21 @@ axios
         let port = response.data.port
 
         const authLink = setContext((_, { headers }) => {
-            const token = localStorage.getItem("phonenumbers-user-token");
+            const token = localStorage.getItem('phonenumbers-user-token')
             return {
                 headers: {
                     ...headers,
                     authorization: token ? `bearer ${token}` : null,
-                },
-            };
-        });
+                }
+            }
+        })
 
-        const httpLink = new HttpLink({ uri: `https://tooter-app.herokuapp.com/ ` });
-
-        const wsLink = new WebSocketLink({
-            uri: `ws://tooter-app.herokuapp.com/graphql`,
-            options: {
-                reconnect: true,
-            },
-        });
-
-        const splitLink = split(
-            ({ query }) => {
-                const definition = getMainDefinition(query);
-                return (
-                    definition.kind === "OperationDefinition" &&
-                    definition.operation === "subscription"
-                );
-            },
-            wsLink,
-            authLink.concat(httpLink)
-        );
+        const httpLink = new HttpLink({ uri: 'https://tooter-app.herokuapp.com/' })
 
         const client = new ApolloClient({
             cache: new InMemoryCache(),
-            link: splitLink,
-        });
-
+            link: authLink.concat(httpLink)
+        })
 
         const root = ReactDOM.createRoot(document.getElementById('root'));
         root.render(
