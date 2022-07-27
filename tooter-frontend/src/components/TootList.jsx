bcb8fabbfe8a9cc9e821f52daeddb63ed91c52a3
DDQuin <ddquin420@gmail.com>
2022-07-27 21:49:52 +0100

    final fix, use port 4000 for dev and full url for deploying

--- tooter-frontend/src/components/TootList.jsx
+++ tooter-frontend/src/components/TootList.jsx
@@ -1,18 +1,9 @@
 import useToots from "../hooks/useToots";
 import Toot from "./Toot";
-import axios from "axios"
-import {useEffect} from "react";
+
 
 const TootList = () => {
-//try to fetch port
     const { toots } = useToots();
-    useEffect(() => {
-        axios
-            .get('/api/test/test')
-            .then(response => {
-                console.log(response)
-            })
-    }, [])
     if (!toots) {
         return (
             <div>
--- tooter-frontend/src/index.js
+++ tooter-frontend/src/index.js
@@ -10,39 +10,33 @@ import {
     InMemoryCache,
 } from "@apollo/client";
 
-import { setContext } from "@apollo/client/link/context";
+import {setContext} from "@apollo/client/link/context";
 
-import axios from "axios";
 
-axios
-    .get('/api/test/test')
-    .then(response => {
-        let port = response.data.port
+const authLink = setContext((_, {headers}) => {
+    const token = localStorage.getItem('toot-token')
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `bearer ${token}` : null,
+        }
+    }
+})
 
-        const authLink = setContext((_, { headers }) => {
-            const token = localStorage.getItem('phonenumbers-user-token')
-            return {
-                headers: {
-                    ...headers,
-                    authorization: token ? `bearer ${token}` : null,
-                }
-            }
-        })
+// for deploy const httpLink = new HttpLink({ uri: `https://tooter-app.herokuapp.com/` })
+const httpLink = new HttpLink({uri: `http://localhost:4000`})
 
-        const httpLink = new HttpLink({ uri: `http://localhost:${port}` })
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink)
+})
 
-        const client = new ApolloClient({
-            cache: new InMemoryCache(),
-            link: authLink.concat(httpLink)
-        })
-
-        const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(
-            <ApolloProvider client={client}>
-                <App />
-            </ApolloProvider>,
-        );
-    })
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>,
+);
 
 
 
