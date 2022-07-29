a1ab0d864ae5a9a707d09cddb0517aadee020d8a
DDQuin <ddquin420@gmail.com>
2022-07-29 20:42:14 +0100

    added all missed files deploy

---
+++ tooter-frontend/src/utils/dateString.js
@@ -0,0 +1,14 @@
+
+const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+
+export default function dateString(time) {
+    const date = new Date(Number(time))
+    const monthString = monthNames[date.getMonth()]
+    const hours = date.getHours()
+    const AmOrPm = hours >= 12 ? 'pm' : 'am';
+    const hourTwelve = (hours % 12) || 12;
+    let dateS = `${monthString} ${date.getDate()} ${hourTwelve}:${date.getMinutes()} ${AmOrPm}`
+    return dateS
+}
