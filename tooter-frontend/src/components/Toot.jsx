5d5d23dd12a6a513750a1e7a855794e8a4ceac9d
DDQuin <ddquin420@gmail.com>
2022-07-29 20:46:17 +0100

    added alt tesxtr for images and removed outside links to test if heroku works

--- tooter-frontend/src/components/Toot.jsx
+++ tooter-frontend/src/components/Toot.jsx
@@ -3,14 +3,14 @@ import dateString from "../utils/dateString";
 
 const Toot = ({toot}) => {
     let avatar = toot.user.avatar
-    if (!avatar) {
+    //if (!avatar) {
         avatar = "/person.png"
-    }
+   // }
 
     return (
         <div className={styles.container}>
             <div className={styles.topPart}>
-                <img className={styles.avatar} src={avatar}/>
+                <img className={styles.avatar} src={avatar} alt={"avatar"}/>
                 <span className={styles.name}>{toot.user.name}</span>
                 <span className={styles.username}>@{toot.user.username}</span>
                 <span className={styles.date}>{dateString(toot.createdAt)}</span>
@@ -20,11 +20,11 @@ const Toot = ({toot}) => {
             </p>
             <div className={styles.interaction}>
                 <div className={styles.likeCon}>
-                    <button className={styles.button}><img className={styles.like} src="/like.png"/></button>
+                    <button className={styles.button}><img  alt={"like"} className={styles.like} src="/like.png"/></button>
                     {toot.likes.length}
                 </div>
                 <div className={styles.commentCon}>
-                    <button className={styles.button}><img className={styles.comment} src="/comment.png"/></button>
+                    <button className={styles.button}><img alt={"comment"} className={styles.comment} src="/comment.png"/></button>
                     {toot.comments.length}
                 </div>
             </div>
