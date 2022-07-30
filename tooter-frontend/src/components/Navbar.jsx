9d61f83270ece241e442c69ff2346d41faf1c2b1
DDQuin <ddquin420@gmail.com>
2022-07-30 20:13:54 +0100

    added alt text to arrow for failing test CI/CD

--- tooter-frontend/src/components/Navbar.jsx
+++ tooter-frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const navigate = useNavigate()
     return (
         <nav className={style.navbar}>
-            <button  onClick={() => navigate(-1)} className={style.buttonArrow}><img className={style.arrow} src={"/images/arrow.png"}/></button>
+            <button  onClick={() => navigate(-1)} className={style.buttonArrow}><img className={style.arrow} src={"/images/arrow.png"} alt={"Arrow"}/></button>
 
             <Link to="/" className={style.link}>Home</Link>
             <Link to="/about" className={style.link}>About</Link>
